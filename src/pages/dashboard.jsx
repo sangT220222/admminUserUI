b7f9a89b165b3f2d6901bd0a8d42defb700418da
Sangam <100322380+sangT220222@users.noreply.github.com>
2025-10-09 17:10:31 +0100

    Additional card added to Dashboard component

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -3,13 +3,14 @@ export default function Dashboard() {
     // p- = padding on all sides
     <div className="p-8">
       <h1 className="text-2xl font-bold mb-4">Dashboard</h1>
-      <div className="grid grid-cols-3 gap-6">
+      <div className="grid grid-cols-4 gap-6">
         {/* grid makes div a parent container */}
         {/* grid-cols-... define 3 equal width columns */}
         {/* gap-6 adds spacing between rows and columns */}
         <div className="bg-white p-4 rounded shadow">Card 1</div>
         <div className="bg-white p-4 rounded shadow">Card 2</div>
         <div className="bg-white p-4 rounded shadow">Card 3</div>
+        <div className="bg-white p-4 rounded shadow">Card 4</div>
       </div>
     </div>
   );
