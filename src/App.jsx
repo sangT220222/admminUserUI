8aa1c53fb97bd63d5acd68337127543fbb3dbb9d
Sangam <100322380+sangT220222@users.noreply.github.com>
2025-10-12 22:27:57 +0100

    Logic applied to sidebar - collapse and expand

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Sidebar from "./components/sidebar";
 import Dashboard from "./pages/dashboard";
+import { useState } from "react";
 
 function App() {
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
   return (
     <div className="flex min-h-screen bg-gray-100 text-gray-800">
-      <Sidebar />
+      <Sidebar isSidebarOpen={isSidebarOpen} setSidebarOpen={setSidebarOpen} />
       <main className="flex-1">
         <Dashboard />
       </main>
--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,35 +1,57 @@
 import { Home, Users, Settings } from "lucide-react";
 
-export default function Sidebar() {
+export default function Sidebar({ isSidebarOpen, setSidebarOpen }) {
   return (
     //aside is used normally for sidebars or call-out boxes
-    <aside className="w-64 bg-gray-900 text-gray-100 h-screen flex flex-col">
+    <aside
+      className={`${
+        isSidebarOpen ? "w-64" : "w-20"
+      } bg-gray-900 text-gray-100 h-screen flex flex-col transition-all duration-300`}
+    >
       {/* //flex-col: makes flex items be arranged vertically, from top to bottom. */}
-      <div className="p-6 text-2xl font-bold border-b border-gray-800">
+      {/* Header / logo section */}
+      <div
+        className={`${
+          isSidebarOpen ? "text-2xl p-6" : "text-sm p-2"
+        } font-bold border-b border-gray-800`}
+      >
         User<span className="text-blue-400">Panel</span>
       </div>
-
+      {/* Navigation links */}
       <nav className="flex-1 p-4 space-y-3">
         <a
           href="#"
-          className="flex items-center gap-3 text-gray-300 hover:text-white"
+          className={`${
+            isSidebarOpen ? "justify-start" : "justify-center"
+          } flex items-center gap-3 text-gray-300 hover:text-white`}
           //gap-3 -> gives gap from the left
         >
-          <Home size={25} /> Dashboard
+          <Home size={25} /> {isSidebarOpen && <span>Dashboard</span>}
+          {/* <Home size={25}/> <span className={isSidebarOpen ? "opacity-100" : "opacity-0 hidden"}>Dashboard</span> */}
         </a>
         <a
           href="#"
-          className="flex items-center gap-3 text-gray-300 hover:text-white"
+          className={`${
+            isSidebarOpen ? "justify-start" : "justify-center"
+          } flex items-center gap-3 text-gray-300 hover:text-white`}
         >
-          <Users size={25} /> Users
+          <Users size={25} /> {isSidebarOpen && <span>Users</span>}
         </a>
         <a
           href="#"
-          className="flex items-center gap-3 text-gray-300 hover:text-white"
+          className={`${
+            isSidebarOpen ? "justify-start" : "justify-center"
+          } flex items-center gap-3 text-gray-300 hover:text-white`}
         >
-          <Settings size={25} /> Settings
+          <Settings size={25} /> {isSidebarOpen && <span>Settings</span>}
         </a>
       </nav>
+      <button
+        onClick={() => setSidebarOpen(!isSidebarOpen)}
+        className="p-4 border-t border-gray-800 text-gray-200 hover:text-white"
+      >
+        {isSidebarOpen ? "Collapse" : "Expand"}
+      </button>
     </aside>
   );
 }
